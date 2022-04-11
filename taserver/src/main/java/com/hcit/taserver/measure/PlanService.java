1c6c581b423b955d50618889fd57fc45d5f22b39
王启航 <2820956950@qq.com>
2022-04-11 10:31:37 +0800

    条件查询

--- taserver/src/main/java/com/hcit/taserver/measure/PlanService.java
+++ taserver/src/main/java/com/hcit/taserver/measure/PlanService.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Predicate;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -21,11 +22,7 @@ public class PlanService {
 
   public Plan findById(Integer id) {
     Plan plan = planRepository.findById(id).orElseThrow();
-    Assessment assessment = assessmentService.findById(plan.getAsmtId());
-    List<Measure> measures = measureService.findAllByPlanId(id);
-    plan.setMeasures(measures);
-    plan.setAssessment(assessment);
-    return plan;
+    return bindData(plan);
   }
 
 
@@ -37,6 +34,25 @@ public class PlanService {
       return query.where(cb.and(deptId, asmtId)).getRestriction();
 
     };
-    return planRepository.findAll(spec);
+    return (List<Plan>)bindData(planRepository.findAll(spec));
   }
+
+  private Plan bindData(Plan input) {
+    Assessment assessment = assessmentService.findById(input.getAsmtId());
+    List<Measure> measures = measureService.findAllByPlanId(input.getId());
+    input.setMeasures(measures);
+    input.setAssessment(assessment);
+    return input;
+  }
+
+  /**
+   * 批量给plan绑定measures和assessment
+   * @param input 未绑定数据的plan
+   * @return 绑定数据后的原对象
+   */
+  private Collection<Plan> bindData(Collection<Plan> input) {
+    input.forEach(this::bindData); // todo: 优化
+    return input;
+  }
+
 }
