169aee89c8c1f24ba12e0472c023c1b3efc3610c
Auly <whitefryfish@gmail.com>
2022-12-05 10:33:19 +0800

    0.3.6 data update

---
+++ taserver/src/main/resources/db_update_0.3.6.sql
@@ -0,0 +1,11 @@
+update fr_matter m
+set m.matter_form_id = 18
+where m.matter_form_id = 15;
+
+update fr_matter m
+set m.matter_form_id = 15
+where m.matter_form_id = 16;
+
+update fr_matter m
+set m.matter_form_id = 16
+where m.matter_form_id = 21;
