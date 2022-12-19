b20550aba2a1a0f6afc8102dbf943965fbe28c22
gcs <1358738595@qq.com>
2022-12-19 17:03:12 +0800

    web:四责系统登录页修改、一单三书和纪委动议文件必传、履责报告form修改（todo）

--- taweb/src/Fr/ReportList/ReportCreateModal.tsx
+++ taweb/src/Fr/ReportList/ReportCreateModal.tsx
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
 import { useHttp } from '../../utils/request';
 import BaseStepForm from '../../components/BaseStepForm';
-import { reportColumns } from './index';
+import { ProColumns } from '@ant-design/pro-table';
+import UserSelectCascader from '../../components/UserSelectCascader';
+import { Select } from 'antd';
+import FileUpload from '../../components/FileUpload';
+import { useAuth } from '../../utils/auth';
 
 type Props = {
   isFirstEdit: boolean,
@@ -12,10 +16,122 @@ type Props = {
 
 export default function ReportCreateModal({ isFirstEdit, id }: Props) {
 
+  const reportType = {
+    1: '民主（组织）生活会前谈心谈话报告',
+    2: '党委（党组织）专题会前集中报告',
+    3: '特殊情况报告',
+    4: '向区（镇）党委、纪委书面报告',
+    5: '向县委、县纪委书面报告',
+    6: '向区（镇）分管领导报告'
+  }
+
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const { state: userState } = useHttp('/user', { initState: [] });
   const { state } = useHttp(`/ordinal/report/${id}`, { initState: {}, isManual: !id });
   const { http: updateHttp } = useHttp(`/ordinal/report/${id}`, { method: 'POST', isManual: true });
   const { http } = useHttp('/ordinal/report', { method: 'POST', isManual: true });
+
+  // 根据用户决定报告类型和监督评议人
+  function setType(user: any) {
+    let option = [], supervisor = [];
+    if (user.id === 1 && user.department?.id === 1) {
+      option = [reportType[5]];
+    } else if (user.id === 2 && user.department?.id === 2) {
+      supervisor = userState.find(u => u.id === 1);
+      option = [reportType[2], reportType[5]];
+    } else if (user.privilege === 'DOUBLE' && user.department?.id === 3) {
+      supervisor = userState.find(u => u.id === 2);
+      option = [reportType[1], reportType[2], reportType[3]];
+    } else if (user.department?.id === 4) {
+      supervisor = userState.find(u => u.id === 1);
+      option = [reportType[2], reportType[5]];
+    } else if (user.department?.id === 5) {
+      supervisor = userState.find(u => u.id === 1);
+      option = [reportType[2]];
+    } else if (user.privilege === 'DEPT' && user.department?.deptType === 'VILLAGE') {
+      supervisor = userState.find(u => u.id === 1);
+      option = [reportType[4]];
+    } else if (user.privilege === 'FIRST' && user.department?.deptType === 'VILLAGE') {
+      supervisor = userState.find(u => u.id === 2);
+      option = [reportType[2], reportType[4]];
+    } else if (user.privilege === 'DOUBLE' && user.department?.deptType === 'VILLAGE') {
+      // todo
+      supervisor = userState.filter(u => u.department?.first);
+      option = [reportType[1], reportType[2], reportType[3]];
+    } else if (user.privilege === 'DEPT_J' && user.department?.deptType === 'VILLAGE') {
+      // todo
+      supervisor = userState.filter(u => u.department?.dept);
+      option = [reportType[2], reportType[4]];
+    } else if (user.privilege === 'DEPT' && user.department?.deptType === 'STATION') {
+      // todo
+      supervisor = userState.filter(u => u.department?.first);
+      option = [reportType[6]]
+    } else if (user.privilege === 'FIRST' && user.department?.deptType === 'STATION') {
+      // todo
+      supervisor = userState.filter(u => u.department?.first);
+      option = [reportType[6]];
+    }
+    return { supervisor, option };
+
+  }
+
+  const reportColumns: ProColumns[] = [
+    {
+      title: '报告人',
+      dataIndex: 'singleUser1',
+      renderText: t => t?.name,
+      renderFormItem: () => <UserSelectCascader disabled value={user} />,
+      formItemProps: { rules: [{ required: true, message: '此项必填' }] },
+    },
+    {
+      title: '报告类型',
+      dataIndex: 'content1',
+      renderFormItem: () => <Select>
+        {
+          setType(user).option?.map((item, index) => <Select.Option
+              value={item}
+              key={index}
+          >
+            {item}
+          </Select.Option>)
+        }
+      </Select>,
+      formItemProps: { rules: [{ required: true, message: '此项必填' }] },
+    },
+
+    {
+      title: '履责情况报告',
+      dataIndex: 'longContent1',
+      valueType: 'textarea',
+      hideInTable: true,
+      formItemProps: { rules: [{ required: true, message: '此项必填' }] },
+    },
+    {
+      title: '报告日期',
+      dataIndex: 'time1',
+      valueType: 'date',
+      formItemProps: { rules: [{ required: true, message: '此项必填' }] },
+    },
+    {
+      title: '监督评议人',
+      dataIndex: 'supervisor',
+      renderFormItem: () => <Select>
+        {
+          setType(user).supervisor?.map(u => <Select.Option value={u.id} key={u.id}>{u.name}</Select.Option>)
+        }
+      </Select>
+    },
+    { title: '上传附件', dataIndex: 'attach', renderFormItem: () => <FileUpload isInEdit />, hideInDescriptions: true },
+    {
+      title: '监督评议主体',
+      dataIndex: 'singleUser2',
+      renderText: t => t?.name,
+      renderFormItem: () => <UserSelectCascader />
+    },
+    { title: '监督评议意见', dataIndex: 'longContent2', valueType: 'textarea', hideInTable: true },
+    { title: '监督评议时间', dataIndex: 'time2', valueType: 'date' },
+  ];
 
   return <>
     <BaseStepForm
