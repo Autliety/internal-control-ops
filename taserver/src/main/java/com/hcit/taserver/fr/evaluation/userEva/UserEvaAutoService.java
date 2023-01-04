4a1bc2dd2fe6ca39701afe80fc3db6a1dd57e30c
Wangqihang <2820956950@qq.com>
2023-01-04 16:04:52 +0800

    系统评分 1&3

--- taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/UserEvaAutoService.java
+++ taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/UserEvaAutoService.java
@@ -1,5 +1,6 @@
 package com.hcit.taserver.fr.evaluation.userEva;
 
+import com.hcit.taserver.common.Status;
 import com.hcit.taserver.department.user.User;
 import com.hcit.taserver.department.user.UserRepository;
 import com.hcit.taserver.fr.evaluation.Evaluation;
@@ -7,13 +8,18 @@ import com.hcit.taserver.fr.matter.Matter;
 import com.hcit.taserver.fr.matter.MatterRepository;
 import com.hcit.taserver.fr.matter.form.MatterForm;
 import com.hcit.taserver.fr.matter.form.MatterFormRepository;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import com.hcit.taserver.fr.meeting.MeetingRepository;
+import com.hcit.taserver.fr.ordinal.FormType;
+import com.hcit.taserver.fr.ordinal.OrdinalForm;
+import com.hcit.taserver.fr.ordinal.OrdinalFormRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,13 +29,15 @@ public class UserEvaAutoService {
   private final UserRepository userRepository;
   private final MatterFormRepository matterFormRepository;
   private final MatterRepository matterRepository;
+  private final MeetingRepository meetingRepository;
+  private final OrdinalFormRepository ordinalFormRepository;
 
   @Scheduled(cron = "0 0 5 * * ? ")
   public void autoEvaluations() {
     List<User> users = userRepository.findAll();
     for (User user : users) {
       var result = new ArrayList<UserEvaluation>();
-      for (long l = 1L; l <= 3; l++) {
+      for (long l = 1L; l <= 21; l++) {
         result.add(userEvaAuto(l, user.getId()));
       }
       userEvaService.updateEvaluation(result, UpdateType.AUTO, user.getId());
@@ -37,32 +45,47 @@ public class UserEvaAutoService {
   }
 
   public UserEvaluation userEvaAuto(long evaId, Long userId) {
-    BigDecimal auto = null;
+    BigDecimal auto = BigDecimal.ZERO;
     if (evaId == 1) {
-      auto = BigDecimal.ZERO;
+      auto = evaluation1(userId);
     }
     if (evaId == 2) {
       auto = evaluation2(userId);
     }
     if (evaId == 3) {
-      auto = BigDecimal.ZERO;
-    }
-    if (auto == null) {
-      throw new IllegalArgumentException();
+      auto = evaluation3(userId);
     }
     return UserEvaluation.builder()
         .id(new Key(User.builder().id(userId).build(), Evaluation.builder().id(evaId).build()))
         .auto(auto).build();
   }
 
-  private BigDecimal evaluation2(Long userId) {
-    // 不到时间满分
-    var now = LocalDate.now();
-    var endTime = now.withMonth(3).withDayOfMonth(31);
-    if (now.isBefore(endTime)) {
-      return new BigDecimal(8);
-    }
+  private BigDecimal evaluation1(Long userId) {
+    int requireTimes = 4;
+    var meetingByUserId = meetingRepository.findAllByMeetingUserId(userId);
+    var meetingSize = meetingByUserId.size();
 
+    var myTopic = meetingByUserId.stream()
+        .flatMap(meeting -> meeting.getTopic().stream())
+        .filter(topic -> Objects.equals(topic.getUser().getId(), userId))
+        .collect(Collectors.toList());
+    var topicSize = myTopic.size();
+    var reviewedSize = myTopic.stream()
+        .filter(topic -> topic.getStatus() == Status.REVIEWED)
+        .count();
+
+    // 4
+    // - max(0, requireTimes - meetingSize) * 0.5
+    // - max(0, requireTime - topicSize) * 0.3
+    // - max(0, requireTimes - reviewedSize) * 0.2
+    return new BigDecimal(40)
+        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - meetingSize)).multiply(new BigDecimal(5)).negate())
+        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - topicSize)).multiply(new BigDecimal(3)).negate())
+        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - reviewedSize)).multiply(new BigDecimal(2)).negate())
+        .divide(BigDecimal.TEN, 2, RoundingMode.HALF_UP);
+  }
+
+  private BigDecimal evaluation2(Long userId) {
     // 无需做问题清单的满分
     MatterForm matterFrom = matterFormRepository.findByUserId(userId);
     if (matterFrom == null) {
@@ -82,6 +105,25 @@ public class UserEvaAutoService {
     }
     scoreMeasure = Math.min(scoreMeasure, 4);
     return new BigDecimal(scoreMatter + scoreMeasure);
+  }
+
+  private BigDecimal evaluation3(Long userId) {
+    var ordinalForms = ordinalFormRepository.findAllByFormTypeAndMultiUser1Id(FormType.LEARNING, userId);
+
+    Set<Integer> months = new HashSet<>();
+    for (OrdinalForm ordinalForm : ordinalForms) {
+      int value = ordinalForm.getCreateTime().getMonth().getValue();
+      months.add(value);
+    }
+
+    // max{0, 2.0 - 0.2 * max[ (12 - size), 0 ]}
+    return BigDecimal.ZERO.max(
+        new BigDecimal(20).add(
+            new BigDecimal(-2).multiply(
+                BigDecimal.ZERO.max(
+                    new BigDecimal(12).add(new BigDecimal(months.size()).negate())))
+        ).divide(BigDecimal.TEN, 2, RoundingMode.HALF_UP)
+    );
   }
 
 }
--- taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/UserEvaService.java
+++ taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/UserEvaService.java
@@ -1,35 +1,16 @@
 package com.hcit.taserver.fr.evaluation.userEva;
 
-import com.hcit.taserver.common.Status;
 import com.hcit.taserver.department.user.AuthService;
 import com.hcit.taserver.department.user.User;
 import com.hcit.taserver.department.user.UserRepository;
-import com.hcit.taserver.fr.matter.Matter;
-import com.hcit.taserver.fr.matter.MatterRepository;
-import com.hcit.taserver.fr.matter.form.MatterForm;
-import com.hcit.taserver.fr.matter.form.MatterFormRepository;
-import com.hcit.taserver.fr.meeting.MeetingRepository;
-import com.hcit.taserver.fr.meeting.topic.MeetingTopicRepository;
-import com.hcit.taserver.fr.ordinal.FormType;
-import com.hcit.taserver.fr.ordinal.OrdinalForm;
-import com.hcit.taserver.fr.ordinal.OrdinalFormRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
@@ -42,11 +23,7 @@ public class UserEvaService {
   private static final BigDecimal SPECIAL_WEIGHT = BigDecimal.valueOf(0.9);
 
   private final UserRepository userRepository;
-  private final MeetingRepository meetingRepository;
   private final UserEvaRepository userEvaRepository;
-  private final MeetingTopicRepository meetingTopicRepository;
-  private final OrdinalFormRepository ordinalFormRepository;
-
   private final AuthService authService;
 
   public List<UserEvaluation> updateEvaluation(List<UserEvaluation> evaluations, UpdateType type, Long destUserId) {
@@ -90,7 +67,7 @@ public class UserEvaService {
     return userEvaRepository.saveAll(result);
   }
 
-  public BigDecimal countTotal(UserEvaluation ue) {
+  private BigDecimal countTotal(UserEvaluation ue) {
     // todo if (ue.getId().getEvaluation().isSpecial()) {} else
     return Optional.ofNullable(ue.getSelf()).orElse(BigDecimal.ZERO).multiply(SELF_WEIGHT)
         .add(
@@ -107,45 +84,4 @@ public class UserEvaService {
     }
   }
 
-  public BigDecimal evaluationUserOne(Long userId) {
-    int requireTimes = 4;
-    var meetingByUserId = meetingRepository.findAllByMeetingUserId(userId);
-    var meetingSize = meetingByUserId.size();
-
-    var myTopic = meetingByUserId.stream()
-        .flatMap(meeting -> meeting.getTopic().stream())
-        .filter(topic -> Objects.equals(topic.getUser().getId(), userId))
-        .collect(Collectors.toList());
-    var topicSize = myTopic.size();
-    var reviewedSize = myTopic.stream()
-        .filter(topic -> topic.getStatus() == Status.REVIEWED)
-        .count();
-
-    // 4
-    // - max(0, requireTimes - meetingSize) * 0.5
-    // - max(0, requireTime - topicSize) * 0.3
-    // - max(0, requireTimes - reviewedSize) * 0.2
-    return new BigDecimal(40)
-        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - meetingSize)).multiply(new BigDecimal(5)).negate())
-        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - topicSize)).multiply(new BigDecimal(3)).negate())
-        .add(BigDecimal.ZERO.max(new BigDecimal(requireTimes - reviewedSize)).multiply(new BigDecimal(2)).negate())
-        .divide(BigDecimal.TEN, 2, RoundingMode.HALF_UP);
-  }
-
-  public BigDecimal evaluationUserThree(Long userId) {
-    var ordinalForms = ordinalFormRepository.findAllByFormTypeAndMultiUser1Id(FormType.LEARNING, userId);
-
-    Set<Integer> months = new HashSet<>();
-    for (OrdinalForm ordinalForm : ordinalForms) {
-      int value = ordinalForm.getCreateTime().getMonth().getValue();
-      months.add(value);
-    }
-
-    // 2.0 - 0.2 * max[ (12 - size), 0 ]
-    return new BigDecimal(20).add(
-        new BigDecimal(-2).multiply(
-            BigDecimal.ZERO.max(
-                new BigDecimal(12).add(new BigDecimal(months.size()).negate())))
-    ).divide(BigDecimal.TEN, 2, RoundingMode.HALF_UP);
-  }
 }
