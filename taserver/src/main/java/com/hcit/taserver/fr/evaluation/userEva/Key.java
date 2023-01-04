f2b4472f55328e2172fbb75206b2997d5c34f464
Auly <whitefryfish@gmail.com>
2023-01-04 14:06:25 +0800

    评分架构调整

--- taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/Key.java
+++ taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/Key.java
@@ -5,8 +5,12 @@ import com.hcit.taserver.fr.evaluation.Evaluation;
 import java.io.Serializable;
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Embeddable
 public class Key implements Serializable {
---
+++ taserver/src/main/java/com/hcit/taserver/fr/evaluation/userEva/UpdateType.java
@@ -0,0 +1,5 @@
+package com.hcit.taserver.fr.evaluation.userEva;
+
+public enum UpdateType {
+  LEADER, SELF, AUTO
+}
