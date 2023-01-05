0735a8a6016416e76b3de30343ce9c0a3b554c1f
Auly <whitefryfish@gmail.com>
2023-01-05 14:52:35 +0800

    eva 4 5 6 17 18 19

--- taserver/src/main/java/com/hcit/taserver/fr/motion/MotionRepository.java
+++ taserver/src/main/java/com/hcit/taserver/fr/motion/MotionRepository.java
@@ -1,8 +1,11 @@
 package com.hcit.taserver.fr.motion;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface MotionRepository extends JpaRepository<Motion, Long>, JpaSpecificationExecutor<Motion> {
+
+  List<Motion> findAllByExecuteUser_Id(Long userId);
 
 }
