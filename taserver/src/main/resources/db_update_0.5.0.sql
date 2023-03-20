29035c21067a95dbfc4c698aaf28bdc018ffc8ab
Auly <whitefryfish@gmail.com>
2023-03-20 10:08:12 +0800

    cleanup client data for open-source prepare.

--- taserver/src/main/resources/db_update_0.5.0.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE fr_evaluation
-  ADD COLUMN year       INT NULL,
-  ADD COLUMN is_special BIT NULL;
-
-UPDATE fr_evaluation SET year = 2022 WHERE page IS NOT NULL;
-
-UPDATE fr_evaluation SET is_special = TRUE WHERE id IN (12, 13, 14, 15, 20, 21);
